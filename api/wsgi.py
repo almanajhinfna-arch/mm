9d5a2eeb9162e2355703156619143778cfd523e9
nihnaandrfama-lang <nihnaandrfama@gmail.com>
2026-03-06 07:58:26 +0800

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -6,7 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``app``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
